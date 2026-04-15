41429b0f205fca6ee2a852b9b8a94ccbb0b7278e
Liam Berigan <48113959+LiamABerigan@users.noreply.github.com>
2026-04-15 03:45:08 -0500

    New HPC processing (step 2.4)

--- methods/penn_shiny_app/prerender_PA_app/deploy_app.R
+++ methods/penn_shiny_app/prerender_PA_app/deploy_app.R
@@ -1,3 +1,4 @@
-source("write_shiny_credentials.R")
-rsconnect::deployApp(appName = "prerender_PA_app", forceUpdate = TRUE)
-
+library(here)
+source(here("methods", "penn_shiny_app", "prerender_PA_app", "write_shiny_credentials.R"))
+rsconnect::deployApp(appDir = here("methods", "penn_shiny_app", "prerender_PA_app"),
+                     appName = "prerender_PA_app", forceUpdate = TRUE)
--- methods/penn_shiny_app/woodcock_weighted_habitat/deploy_app.R
+++ methods/penn_shiny_app/woodcock_weighted_habitat/deploy_app.R
@@ -1,2 +1,4 @@
-source("write_shiny_credentials.R")
-rsconnect::deployApp(appName = "W-PAST2", forceUpdate = TRUE)
+library(here)
+source(here("methods", "penn_shiny_app", "woodcock_weighted_habitat", "write_shiny_credentials.R"))
+rsconnect::deployApp(appDir = here("methods", "penn_shiny_app", "woodcock_weighted_habitat"),
+                     appName = "W-PAST2", forceUpdate = TRUE)
